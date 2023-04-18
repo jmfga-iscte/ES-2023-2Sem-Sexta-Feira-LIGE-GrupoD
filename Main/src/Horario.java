f883c8ed9f06804b29316c72c1f47c19eb9769ff
jmfga-iscte <jmfga@iscte-iul.pt>
2023-04-18 22:50:06 +0100

    Inclusao da classe Horario e respetivos metodos

---
+++ Main/src/Horario.java
@@ -0,0 +1,26 @@
+public class Horario {
+	
+	private void carregarCsv(){
+
+    }
+
+    private void carregarJson(){
+
+    }
+
+    private void gravarCsv(){
+
+    }
+
+    private void gravarJson(){
+
+    }
+    
+    private void converterFicheiro() {
+    	
+    }
+    
+    public static void main(String[] args) {
+    	
+    }
+}
